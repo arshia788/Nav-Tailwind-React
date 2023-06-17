9003fc412a6a647ecb28862a776aaad85ad5e4bf
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-17 21:23:42 +0330

    finished the icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,18 +4,36 @@ import { FaHamburger } from "react-icons/fa";
 import { BsFillXCircleFill } from "react-icons/bs";
 
 const Nav = () => {
+
+    const [show, setShow]= useState(false);
+
     return (
         <nav className='bg-blue-950 text-white py-2 px-4 flex justify-between items-center'>
 
-            <h2 className='text-2xl font-bold'>logo</h2>
+            <h2 className='text-3xl font-bold '>logo</h2>
 
             <div>
-                <ul className='flex gap-x-3'>
+                <ul className='flex gap-x-3 text-md'>
                     <li>home</li>
                     <li>shop</li>
                     <li>log-out</li>
                 </ul>
             </div>
+
+            {
+                show
+                ?
+                <i onClick={()=> setShow(!show)} className='xs:block cursor-pointer sm:hidden text-3xl'>
+                    <BsFillXCircleFill />
+                </i>
+                :
+                <i onClick={()=> setShow(!show)} className='xs:block cursor-pointer  sm:hidden text-3xl'>
+                    <FaHamburger />
+                </i>
+                
+            }
+
+            
             
         </nav>
     );
