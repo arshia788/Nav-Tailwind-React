d72c48ffd933ccfbd016cf1daaaecc5bcb65d587
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-17 21:16:11 +0330

    added the icons and working on styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import { FaHamburger } from "react-icons/fa";
+import { BsFillXCircleFill } from "react-icons/bs";
 
 const Nav = () => {
     return (
-        <div>
+        <nav className='bg-blue-950 text-white py-2 px-4 flex justify-between items-center'>
+
+            <h2 className='text-2xl font-bold'>logo</h2>
+
+            <div>
+                <ul className='flex gap-x-3'>
+                    <li>home</li>
+                    <li>shop</li>
+                    <li>log-out</li>
+                </ul>
+            </div>
             
-        </div>
+        </nav>
     );
 };
 
